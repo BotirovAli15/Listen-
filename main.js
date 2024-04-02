394522973da1de2e4e91e8cf4d3eb0c3e9eb739f
BotirovAli15 <164502551+BotirovAli15@users.noreply.github.com>
2024-04-02 18:54:04 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+function openYouTube() {
+    window.open('https://www.youtube.com', 'blank');
+}
+
+function openChrome() {
+    window.open('https://www.google.com/', 'blank');
+}
+
+function openSpace() {
+    window.open('https://space.marsit.uz/main#active-course', 'blank');
+}
+
+
+const recognitionRU = new webkitSpeechRecognition();
+recognitionRU.lang = 'ru-RU'; 
+
+const recognitionEN = new webkitSpeechRecognition();
+recognitionEN.lang = 'en-US'; 
+
+const recognizeSpeech = (recognition) => {
+    recognition.onresult = function(event) {
+        const transcript = event.results[0][0].transcript.toLowerCase(); 
+        console.log('Распознанная речь:', transcript);
+
+        if (transcript.includes('открой youtube')) {
+            openYouTube();
+        }if(transcript.includes('открой гугл')){
+            openChrome();
+        }if(transcript.includes("open chrome")){
+            openChrome();
+        }if(transcript.includes("open youtube")){
+            openYouTube();
+        }if(transcript.includes('открой space')){
+            openSpace();
+        }if(transcript.includes('open space')){
+            openSpace();
+        }
+        
+    };
+}
+
+document.getElementById('start-button').addEventListener('click', function() {
+    recognizeSpeech(recognitionRU);
+    recognitionRU.start();
+});
+
+document.getElementById('start-button-en').addEventListener('click', function() {
+    recognizeSpeech(recognitionEN);
+    recognitionEN.start();
+});
+
+window.onload = function() {
+    recognizeSpeech(recognitionRU);
+    recognitionRU.start();
+};
